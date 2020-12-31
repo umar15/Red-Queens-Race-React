d939b3194ea4d78900ee5f0a5de2a8063893115d
Umar Khalid <umar.qur105@gmail.com>
2020-12-31 20:49:32 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import useWebAnimations from "@wellyshen/use-web-animations";
 
 function App() {
-	var scenaryFrames = [
-		{ transform: "transalteX(100%" },
-		{ transform: "transalteX(-100%" },
+	var sceneryFrames = [
+		{ transform: "translateX(100%)" },
+		{ transform: "translateX(-100%)" },
 	];
 
 	var sceneryTimingBackground = {
@@ -13,86 +13,164 @@ function App() {
 		iterations: Infinity,
 	};
 
-	const { ref } = useWebAnimations({
-		keyframes: scenaryFrames,
+	var sceneryTimingForeground = {
+		duration: 12000,
+		iterations: Infinity,
+	};
+
+	const alice = useWebAnimations({
+		keyframes: [
+			{ transform: "translateY(0)" },
+			{ transform: "translateY(-100%)" },
+		],
+		timing: {
+			easing: "steps(7, end)",
+			direction: "reverse",
+			duration: 200,
+			playbackRate: 1,
+			iterations: Infinity,
+		},
+	});
+
+	const background1 = useWebAnimations({
+		keyframes: sceneryFrames,
 		timing: sceneryTimingBackground,
 	});
+
+	const background2 = useWebAnimations({
+		keyframes: sceneryFrames,
+		timing: sceneryTimingBackground,
+	});
+
+	const foreground1 = useWebAnimations({
+		keyframes: sceneryFrames,
+		timing: sceneryTimingForeground,
+	});
+
+	const foreground2 = useWebAnimations({
+		keyframes: sceneryFrames,
+		timing: sceneryTimingForeground,
+	});
+
+	useEffect(() => {
+		alice.getAnimation().updatePlaybackRate(0.1);
+		alice.getAnimation().duration = 600;
+
+		document.addEventListener("mousedown", (e) => {
+			goFaster();
+		});
+
+		document.addEventListener("touchstart", (e) => {
+			goFaster();
+		});
+
+		var sceneries = [
+			background1.getAnimation(),
+			background2.getAnimation(),
+			foreground1.getAnimation(),
+			foreground2.getAnimation(),
+		];
+
+		var adjustBackgroundPlayback = function () {
+			if (alice.getAnimation().playbackRate < 0.8) {
+				sceneries.forEach(function (anim) {
+					anim.playbackRate = alice.getAnimation().playbackRate * 2;
+				});
+			} else if (alice.getAnimation().playbackRate > 1.2) {
+				sceneries.forEach(function (anim) {
+					anim.playbackRate = alice.getAnimation().playbackRate * 2.5;
+				});
+			}
+		};
+
+		setInterval(function () {
+			if (alice.getAnimation().playbackRate > 0.4) {
+				alice.getAnimation().playbackRate *= 0.9;
+			}
+			adjustBackgroundPlayback();
+		}, 3000);
+
+		var goFaster = function () {
+			alice
+				.getAnimation()
+				.updatePlaybackRate(alice.getAnimation().playbackRate * 1.1);
+			adjustBackgroundPlayback();
+		};
+	}, [alice.animate]);
 
 	return (
 		<div className="wrapper">
 			<div className="sky"></div>
-
 			<div className="earth">
-				<div className="red-queen-and-alice">
+				<div id="red-queen-and-alice">
 					<img
+						ref={alice.ref}
+						id="red-queen_and_alice_sprite"
 						src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
-						alt="red-queen_and_alice_sprite"
 						srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
+						alt="Alice and the Red Queen running to stay in place."
 					/>
 				</div>
 			</div>
 
-			{/* <div className="scenery foreground1">
+			<div ref={foreground2.ref} className="scenery" id="foreground1">
 				<img
-					className="palm3"
+					id="palm3"
 					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
+					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"
 					alt=" "
-					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png 2x"
 				/>
 			</div>
-
-			<div className="scenery foreground2">
+			<div ref={foreground1.ref} className="scenery" id="foreground2">
 				<img
-					className="bush"
+					id="bush"
 					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"
+					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x"
 					alt=" "
-					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png 2x"
 				/>
 				<img
-					className="w-rook-upright"
+					id="w-rook-upright"
 					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png"
+					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x"
 					alt=" "
-					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png 2x"
 				/>
-			</div> */}
-
-			<div className="scenery background1">
+			</div>
+			<div ref={background1.ref} className="scenery" id="background1">
 				<img
-					className="r-pawn-upright"
+					id="r-pawn-upright"
 					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
 					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x"
 					alt=" "
 				/>
 				<img
-					className="w-rook"
+					id="w-rook"
 					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"
 					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x"
 					alt=" "
 				/>
 				<img
-					className="palm1"
+					id="palm1"
 					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
 					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x"
 					alt=" "
 				/>
 			</div>
-
-			<div className="scenery background2">
+			<div ref={background2.ref} className="scenery" id="background2">
 				<img
-					className="r-pawn"
+					id="r-pawn"
 					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png"
 					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x"
 					alt=" "
 				/>
 
 				<img
-					className="r-knight"
+					id="r-knight"
 					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png"
 					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x"
 					alt=" "
 				/>
 				<img
-					className="palm2"
+					id="palm2"
 					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png"
 					srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"
 					alt=" "
